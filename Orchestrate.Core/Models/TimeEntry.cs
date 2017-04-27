64e25d647afb9e6da5c00955029b41b596652ae5
joeshepherdmba <joeshepherdmba@outlook.com>
2017-04-27 12:09:29 -0700

    updated FK

--- Orchestrate.Core/Models/TimeEntry.cs
+++ Orchestrate.Core/Models/TimeEntry.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using static Orchestrate.Core.Models.Enums;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace Orchestrate.Core.Models
 {
@@ -7,13 +9,21 @@ namespace Orchestrate.Core.Models
 	{
         public int ID { get; set; }
         public string Notes { get; set; }
-        public User User { get; set; }
+
+        [ForeignKey("User")]
+        public int UserID { get; set; }
+        public virtual User User { get; set; }
+
         public int Minutes { get; set; }
         public DateTime Timestamp { get; set; }
-        public User Approver { get; set; }
-        public TimeStatus Status { get; set; }
-        public int TaskID { get; set; }
 
-        public Task Task { get; set; }
+        [ForeignKey("Approver")]
+        public int ApproverID { get; set; }
+        public virtual User Approver { get; set; }
+        public TimeStatus Status { get; set; }
+
+        [ForeignKey("Task")]
+        public int TaskID { get; set; }
+        public virtual Task Task { get; set; }
     }
 }
